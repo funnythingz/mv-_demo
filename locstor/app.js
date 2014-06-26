f1529f1264386b5b10a2a595c00a327f5d855965
funnythingz <hiroki@hackleberry.me>
2014-06-26 16:33:13 +0900

    try Locstor.js

---
+++ locstor/app.js
@@ -0,0 +1,10 @@
+var hoge = {
+    todos: [
+        {id: 1, task: 'hoge1'},
+        {id: 2, task: 'hoge2'},
+        {id: 3, task: 'hoge3'}
+    ]
+}
+
+Locstor.set("hoge", hoge);
+Locstor.get("todos");
